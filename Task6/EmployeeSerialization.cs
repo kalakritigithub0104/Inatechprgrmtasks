7e48eb54a517327aea9780f9caec3a256d21232c
kalakritigithub0104 <113574700+kalakritigithub0104@users.noreply.github.com>
2023-01-28 16:55:39 +0530

    Add files via upload

---
+++ Task6/EmployeeSerialization.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class EmployeeSerialization
+    {
+        public int EmpId { get; set; }
+        public string? EmpName { get; set; }
+        public string? EmpGender { get; set; }
+        public int EmpAge { get; set; }
+
+        public void readfEmp()
+        {
+            FileStream emp = new FileStream("C:\\Inatechprograms\\EmpSer.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader sr = new StreamReader(emp);
+            sr.BaseStream.Seek(0, SeekOrigin.Begin);
+            string str = sr.ReadToEnd();
+            Console.WriteLine(str);
+        }
+        public static void Main(string[] args)
+        {
+            EmployeeSerialization emp = new EmployeeSerialization();
+            Console.WriteLine("Enter Employee Name");
+            emp.EmpName = Console.ReadLine();
+            Console.WriteLine("Enter Employee Gender");
+            emp.EmpGender = Console.ReadLine();
+            Console.WriteLine("Enter Employee ID:");
+            emp.EmpId = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Employee Age:");
+            emp.EmpAge = Convert.ToInt32(Console.ReadLine());
+
+            FileStream Emp = new FileStream(@"C:\Inatechprograms\EmpSer.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(Emp);
+            sw.Write(emp.EmpName);
+            sw.Write(emp.EmpGender);
+            sw.Write(emp.EmpAge);
+            sw.Write(emp.EmpId);
+            sw.Flush();
+            sw.Close();
+            Emp.Close();
+            emp.readfEmp();
+        }
+    }
+
+
+}
+    
+
---
+++ Task6/RestaurantDeserts.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task6
+{
+    internal class RestaurantDeserts
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                List<string> desertList = new List<string>();
+
+                desertList.Add("Ice Cream");
+                desertList.Add("Fruit Salad");
+                desertList.Add("Cheesecake");
+                desertList.Add("Chocolate Cake");
+                desertList.Add("Strawberry Shortcake");
+
+                string fileName = "RestaurantDeserts.txt";
+                FileStream fs = new FileStream(fileName, FileMode.Create);
+                StreamWriter sw = new StreamWriter(fs);
+
+                foreach (string des in desertList)
+                {
+                    sw.WriteLine(des);
+                }
+
+                sw.Close();
+                fs.Close();
+
+                FileInfo fi = new FileInfo(fileName);
+                Console.WriteLine("File Name: {0}", fi.Name);
+                Console.WriteLine("File Creation Time: {0}", fi.CreationTime);
+                Console.WriteLine("File Size: {0} bytes", fi.Length);
+                Console.WriteLine("\nDeserts in the Restaurant:");
+
+                StreamReader sr = new StreamReader(fileName);
+                string desert;
+
+                while ((desert = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(desert);
+                }
+
+                sr.Close();
+
+               
+            }
+        }
+
+    }
+}
